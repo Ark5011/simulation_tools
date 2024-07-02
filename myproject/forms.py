2c8c1cda635cb896c671090b52796bcb0b931041
Ark5011 <shawn.lee.2022@scis.smu.edu.sg>
2024-07-02 11:43:03 +0800

    chore: Update G&T simulation chart and form handling

--- myproject/forms.py
+++ myproject/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 from tg_couchman.models import Project
 
-class Projectform(forms.ModelForm):
-
+class Projectform(forms.ModelForm):     
     class Meta:
         model = Project
         fields = '__all__'
